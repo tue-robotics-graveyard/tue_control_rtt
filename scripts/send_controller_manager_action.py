b22824436c96be0e95bd8c0a9b1c4e444dc0ed40
Rein Appeldoorn <reinzor@gmail.com>
2015-12-15 18:03:00 +0100

    added test script for manager actions

---
+++ scripts/send_controller_manager_action.py
@@ -0,0 +1,29 @@
+#! /usr/bin/python
+
+import sys
+import rospy
+from tue_control_rtt_msgs.msg import ControllerManagerAction, ControllerAction
+
+rospy.init_node("send_controller_action")
+
+args = sys.argv[1:]
+actions = dict(zip(args[0::2],args[1::2]))
+
+if not actions:
+    rospy.logwarn("Usage: send_controller_action CONTROLLER_NAME_1 CONTROLLER_ACTION_1 [CONTROLLER_NAME_2 CONTROLLER_ACTION_2]") 
+    sys.exit(1)
+
+pub = rospy.Publisher("action", ControllerManagerAction, queue_size=10)
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+rospy.loginfo("Sending the the following actions to the controller manager (topic=action):")
+
+msg = ControllerManagerAction()
+for name, action in actions.iteritems():
+    rospy.loginfo("%s --> %s" % (name, action))
+    msg.actions.append(ControllerAction(name=name, action=action))
+
+rospy.sleep(0.5)
+pub.publish(msg)
